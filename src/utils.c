3e7a38974b4a4d3cd52c5b710bb4c17e4222a9d6
CarlosVSL <carlosvsanchezlopez@gmail.com>
2025-06-02 18:15:24 +0200

    Reverted

--- src/utils.c
+++ src/utils.c
@@ -21,11 +21,12 @@ uint64_t	get_time(void)
 	return ((uint64_t)tv.tv_sec * 1000 + tv.tv_usec / 1000);
 }
 
-int	ft_usleep(useconds_t ms)
+int	ft_usleep(useconds_t time)
 {
-	if (ms > 2)
-		usleep((ms - 2) * 1000);
-	while (get_time() + 1 < ms + get_time())
-		usleep(200);                 // 0,2 ms
+	uint64_t	start;
+
+	start = get_time();
+	while (get_time() - start < time)
+		usleep(time / 10);
 	return (0);
 }
